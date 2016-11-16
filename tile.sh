b02ac0484d67dd163cdd926a60c54c70a68d2265
Johnathan Leppert <johnathan@kespry.com>
2016-11-16 02:00:02 -0800

    initial commit

---
+++ tile.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+# this script takes a huge image and creates a tiled map at various zoom levels
+# usage: ./tile.sh <input> <width> <height> <min zoom> <max zoom> 
+# ex: ./tile.sh huge.jpg 8408 8337 18 20 
+inputFile=$1
+width=$2
+height=$3
+minZoom=$4
+maxZoom=$5
+tileSize=256
+
+levels=$(( $maxZoom - $minZoom ))
+for zoom in $(seq 0 $levels); do
+  currentZoom=$(( $maxZoom - $zoom ))
+  echo "Creating map for zoom level $currentZoom..."
+  mkdir $currentZoom
+  cd $currentZoom
+  scale=$(( 1 << ($maxZoom - $currentZoom) ))
+  bcCmd="bc -l <<< '(1 / $scale)*100'"
+  bcScale=$(eval $bcCmd)
+  scaledOutput=$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c 32)
+
+  scaleCmd="convert ../${inputFile} -scale ${bcScale}% ${scaledOutput}.jpg"
+  echo "Resizing image for this zoom level.."
+  eval $scaleCmd
+  convertCmd="convert ${scaledOutput}.jpg -crop ${tileSize}x${tileSize}! -gravity NorthWest -extent ${tileSize}x${tileSize} -transparent white tiles_%d.png"
+  echo "Creating tiles..."
+  eval $convertCmd
+
+  tileRegex="tiles_([0-9]+).png"
+  for f in *.png
+  do
+    if [[ $f =~ $tileRegex ]]; then
+      index="${BASH_REMATCH[1]}"
+      tileWidth=$(echo "$width $tileSize" | awk '{print int( ($1/$2) + 1 )}')
+      x=$(( $index % $tileWidth ))
+      y=$(( $index / $tileWidth ))
+      mvCmd="mv $f map_${x}_${y}.png"
+      eval $mvCmd
+    fi 
+  done
+
+  echo "Done creating map for zoom level $currentZoom."
+
+  cd ../
+done
