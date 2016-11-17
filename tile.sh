54d7aca8f38f69ad922bc90d461e805d1fa38053
Johnathan Leppert <johnathan@kespry.com>
2016-11-16 19:31:54 -0800

    remove debug

--- tile.sh
+++ tile.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 # this script takes a huge image and creates a tiled map at various zoom levels
-# usage: ./tile.sh <input> <width> <height> <min zoom> <max zoom> 
-# ex: ./tile.sh huge.jpg 8408 8337 18 20 
+# usage: ./tile.sh <input> <width> <height> <min zoom> <max zoom> <tileSize> 
+# ex: 
+# ./tile.sh huge.jpg 8408 8337 18 20 256
+# this generates tiles of the image huge.jpg (8408x8337) for zoom levels 18-20 with a tile size of 256x256
 inputFile=$1
 width=$2
 height=$3
 minZoom=$4
 maxZoom=$5
-tileSize=256
+tileSize=$6
 
 levels=$(( $maxZoom - $minZoom ))
 echo $levels
@@ -24,15 +26,9 @@ for zoom in $(seq 0 $levels); do
   scaleCmd="convert ../${inputFile} -scale ${bcScale}% ${scaledOutput}.jpg"
   echo "Resizing image for this zoom level.."
   eval $scaleCmd
-  echo $bcScale
 
   scaledWidth=$(printf "%.0f" $(eval "bc  <<< 'scale=2; ($bcScale / 100) * $width'"))
   scaledHeight=$(printf "%.0f" $(eval "bc <<< 'scale=2; ($bcScale / 100) * $height'"))
-
-  echo "-----"
-  echo $scaledWidth
-  echo $scaledHeight
-  echo "-----"
 
   convertCmd="convert ${scaledOutput}.jpg -crop ${tileSize}x${tileSize}! -gravity NorthWest -extent ${tileSize}x${tileSize} -transparent white tiles_%d.png"
   echo "Creating tiles..."
